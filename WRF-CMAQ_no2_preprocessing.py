5d5452d36d81b0646edd3b540d40b56c285cceb0
Carlos Carrillo-Gallegos <90050144+CarlosECarrilloGallegos@users.noreply.github.com>
2023-08-09 12:21:12 -0400

    Update WRF-CMAQ_no2_preprocessing.py

--- WRF-CMAQ_no2_preprocessing.py
+++ WRF-CMAQ_no2_preprocessing.py
@@ -4,6 +4,8 @@ import numpy as np
 import glob
 import dask
 
+#path code is in: /data0/shr/bne/jc_data/nox/daily
+
 time = ['2005','2006','2007','2008','2009',
         '2010','2011','2012','2013','2014','2015','2016','2017','2018']
 
