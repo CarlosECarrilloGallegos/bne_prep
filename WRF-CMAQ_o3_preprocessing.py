f153b3b08cdc896ca5f374a0d1d471021c8be4fa
Carlos Carrillo-Gallegos <90050144+CarlosECarrilloGallegos@users.noreply.github.com>
2023-08-09 12:13:17 -0400

    Create WRF-CMAQ_o3_preprocessing.py

---
+++ WRF-CMAQ_o3_preprocessing.py
@@ -0,0 +1,20 @@
+import xarray as xr
+import pandas as pd
+import numpy as np
+import glob
+import dask
+
+#path: /data0/shr/bne/jc_data/o3
+
+time = ['2005','2006','2007','2008','2009',
+        '2010','2011','2012','2013','2014','2015','2016','2017','2018']
+
+for i in time:
+    filelist = sorted(glob.glob('/data0/shr/bne/jc_data/o3/daily/raw/WRF-CMAQ/ftp.acom.ucar.edu/user/lacey/regional_atlas_cmaq/dailyaverage/*' + i + '*O3*.nc'))
+    wrf_cmaq = xr.open_mfdataset(filelist, concat_dim = 'None', combine = 'nested')
+    wrf_df = wrf_cmaq.to_dataframe()
+    
+    wrf_df = wrf_df.reset_index()
+    wrf_df = wrf_df.drop('None',axis=1).drop('Time',axis=1).drop('south_north',axis=1).drop('west_east',axis=1).rename(columns={'O3_daily_avg':'pred_WRF-CMAQ'})
+    
+    wrf_df.to_csv('/data0/shr/bne/jc_data/o3/daily/formatted/uncropped/WRF-CMAQ/WRF-CMAQ_o3_' + i + '_daily.csv')
